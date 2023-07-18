e2035a57f18b903b05136692427fe7f4bb728a58
Hugo Mulana Christon <hugo.mulana@yahoo.com>
2023-07-19 00:19:00 +0700

    Adding .env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_BE: string;
+  readonly VITE_APP_PEER: string;
+  readonly VITE_APP_PEER_PORT: string;
+  // more env variables...
+}
